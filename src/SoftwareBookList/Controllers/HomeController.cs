2ad4cbe91e02e4d6a4421d0730881116f6904974
Tou Chang <tchang@students.ntc.edu>
2023-09-11 09:24:33 -0500

    Have a rough draft of the About and Home page with some basic Css for now.

--- src/SoftwareBookList/Controllers/HomeController.cs
+++ src/SoftwareBookList/Controllers/HomeController.cs
@@ -15,4 +15,14 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
 }
