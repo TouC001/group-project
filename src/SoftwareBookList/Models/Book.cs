c9cfb1d350e593f094e3419c323146302026183f
smbohan <smbohan@students.ntc.edu>
2023-09-18 12:48:21 -0500

    Added Book Model class to solution

---
+++ src/SoftwareBookList/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace SoftwareBookList.Models
+{
+    public class Book
+    {
+        public string GoogleID { get; set; }
+        public string Title { get; set; }
+        public string CoverImage { get; set; }
+        public string Author { get; set; }
+        public decimal Rating { get; set; }
+    }
+}
